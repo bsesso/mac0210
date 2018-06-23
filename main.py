5b03b91393e93b0a462ca08527abfb12ad454477
Bruno <bruno.sesso@usp.br>
2018-06-23 17:13:01 -0300

    Suporte para tempo nas medições. Os pontos medidos aparecem no plot
    Antes estava considerando o tempo das medições como o indice do vetor
    que ela se encontrava para fazer as contas. Adicionei suporte para
    tempos. Agora o vetor de medições é um vetor de tuplas (tempo, medição).

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Spline_Interpolator:
       self.n = self.calculate_n(measurements)
     self.lmbda = lmbda
 
-    self.gammas = Gammas(self.n, self.xmin, self.xmax)
+    self.gammas = Gammas(self.n, measurements, self.xmin, self.xmax)
     # M, b = self.calculate_m_b(self.n, gammas, measurements)
     M = self.calculate_M(measurements)
     b = self.calculate_b(measurements)
@@ -42,21 +42,22 @@ class Spline_Interpolator:
     m = len(measurements)
     b = np.zeros((1, self.n))
     for j in range(m):
-      b += measurements[j] * self.gammas.get(j).T
+      b += measurements[j][1] * self.gammas.get(j).T
     b = 2 * b.T
     return b
 
 class Gammas:
-  def __init__(self, n_splines, xmin = 0, xmax = 1):
+  def __init__(self, n_splines, measurements, xmin = 0, xmax = 1):
     self.splines = Spline(np.ones(n_splines), xmin, xmax)
     self.n = n_splines
+    self.tjs = [msmnt[0] for msmnt in measurements]
 
     self.current = -1
     self.array = None
 
   def get(self, j):
     if j != self.current:
-      self.array = np.array([[self.splines.beta_j(i, j) for i in range(self.n)]]).T
+      self.array = np.array([[self.splines.beta_j(i, self.tjs[j]) for i in range(self.n)]]).T
       self.current = j
     return self.array
 
@@ -66,15 +67,23 @@ def plot_curve(curve):
   # Adiciono 0.3 para ver no plot os lados além do interpolado mesmo
   t = np.arange(curve.x_min - 0.3, curve.x_max + 0.3, dt)  
   plt.plot(t, curve(t))
-  plt.show()
+
+def plot_measurements(measurements):
+  t = [msmnt[0] for msmnt in measurements]
+  y = [msmnt[1] for msmnt in measurements]
+
+  plt.plot(t, y, 'ro')
 
 if __name__ == '__main__':
   n_spl = 10
   xmin = 0
   xmax = 10
-  msmnts = np.array([0, 1, 2, 3, 4, 3, 2, 5, 9, 6, 4, 3, 2, 1])
+  msmnts = np.array([(0, 0), (0.2, 0), (0.3, 0), (0.4, 0), (0.5, 0), (0.6, 0), (4, 4), (5, 3), (6, 2), (7, 5), (8, 9), (9, 6), (9.1, 4), (9.2, 3), (9.3, 2), (10, 1)])
 
   ip = Spline_Interpolator(xmin, xmax)
 
-  curve = ip.interpolate(msmnts)
+  curve = ip.interpolate(msmnts, lmbda=50)
+
+  plot_measurements(msmnts)
   plot_curve(curve)
+  plt.show()
