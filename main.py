81ce0b1e5b83d254897d72da903c06bde1047904
Bruno <bruno.sesso@usp.br>
2018-06-23 14:01:55 -0300

    Adicionei a nova implementação de Splines do professor
    Mudei o arquivo splines e alterei a implementação das splines básicas
    para ao invés de criar n splines com pesos (0...0, 1, 0...0) criar
    somente uma spline de peso (1 ... 1) e acessa cada spline dessa através
    do novo método beta_j

--- main.py
+++ main.py
@@ -2,33 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from splines import Spline
 
-def interpolate(n_splines, measurements, xmin = 0, xmax = 1):
-  splines = generate_basic_splines(n_splines, xmin, xmax)  
-  M, b = calculate_m_b(n_splines, splines, measurements)
+def interpolate(n_splines, measurements, xmin = 0, xmax = 1):  
+  M, b = calculate_m_b(n_splines, measurements)
   a = np.linalg.solve(M, 0.5 * b)
   final_curve = Spline(a, xmin, xmax)
   plot_curve(final_curve)
 
-def generate_basic_splines(n, xmin, xmax):
-  splines = []
-
-  ## Cria cada spline 
-  for i in range(n):
-    a = np.array([0] * n)
-    a[i] = 1
-
-    spl = Spline(a, xmin, xmax)
-    splines.append(spl)
-  return splines
-
-def calculate_m_b(n, splines, measurements):
+def calculate_m_b(n, measurements):
   m = len(measurements)
   M = np.zeros((n, n))
   b = np.zeros((1, n))
+  splines = Spline(np.ones(n), 0, 10)
 
   ## Calculate m = sum of gamma * gammaT && b = 2 * sum measurementsj * gammajT
   for j in range(m):
-    gamma = np.array([[spl(j) for spl in splines]]).T
+    gamma = np.array([[splines.beta_j(i, j) for i in range(n)]]).T
     b += measurements[j] * gamma.T
     M += gamma.dot(gamma.T)
   b = 2 * b.T
@@ -43,7 +31,7 @@ def plot_curve(curve):
   plt.show()
 
 if __name__ == '__main__':
-  n_spl = 8
+  n_spl = 10
   xmax = 10
   msmnts = np.array([0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1])
   interpolate(n_splines=n_spl, measurements=msmnts, xmax=xmax)
